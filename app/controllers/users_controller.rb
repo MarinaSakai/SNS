0f09d40b88c16d330a77f26a7728e6631e83a932
Marina S <marina_s@fukazawa.info.waseda.ac.jp>
2019-05-24 17:57:47 +0900

    implemented user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,28 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find_by(params[:id])
+    @user = User.find_by(id: params[:id])
     # 自分がフォローしている人たちのリスト
-    @follows = Follow.select("target_user_id").where(user_id: current_user)
-
+    @follows = Follow.where(user_id: current_user)
+    @follows_id = @follows.pluck(:target_user_id)
     # 自分のプロフィール
     if @user == current_user
       @posts = Post.where(user_id: @user)
     # 他人のプロフィール
     else
       # 自分がフォローしている人だったら公開範囲がフォロワーの投稿も表示
-      if @follows.include?(@user)
-        @posts = Post.where.not(scope_of_disclosure: "self")
+
+      if @follows_id.include?(@user.id)
+        @posts = Post.where(user_id: @user).where(scope_of_disclosure: "everyone")
+        .or(Post.where(user_id: @user).where(scope_of_disclosure: "followers"))
+        .order(created_at: 'desc')
       else
-        @posts = Post.where(scope_of_disclosure: "everyone")
+        @posts = Post.where(user_id: @user).where(scope_of_disclosure: "everyone")
+        .order(created_at: 'desc')
       end
     end
   end
-  
+
+  def edit
+    @user = User.find_by(id: current_user)
+  end
 end
