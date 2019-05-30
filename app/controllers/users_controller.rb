0ba5b8411a9d19ff2ef68b33eff55e46b4bd8eb3
Marina S <marina_s@fukazawa.info.waseda.ac.jp>
2019-05-30 14:22:19 +0900

    Changed the method to not take the list of followers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,14 @@
 class UsersController < ApplicationController
   def show
+    # 見たい人
     @user = User.find_by(id: params[:id])
-    # 自分がフォローしている人たちのリスト
-    @follows = Follow.where(user_id: current_user)
-    @follows_id = @follows.pluck(:target_user_id)
     # 自分のプロフィール
     if @user == current_user
       @posts = Post.where(user_id: @user)
     # 他人のプロフィール
     else
       # 自分がフォローしている人だったら公開範囲がフォロワーの投稿も表示
-
-      if @follows_id.include?(@user.id)
+      if Follow.find_by(user_id: current_user, target_user_id: @user.id)
         @posts = Post.where(user_id: @user).where(scope_of_disclosure: "everyone")
         .or(Post.where(user_id: @user).where(scope_of_disclosure: "followers"))
         .order(created_at: 'desc')
