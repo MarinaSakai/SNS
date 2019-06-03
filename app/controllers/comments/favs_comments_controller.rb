834362b3f198dab13f3cf394eff4521db62d2240
Marina S <marina_s@fukazawa.info.waseda.ac.jp>
2019-06-04 00:56:22 +0900

    implemented comment fav function

--- app/controllers/comments/favs_comments_controller.rb
+++ app/controllers/comments/favs_comments_controller.rb
@@ -1,2 +1,22 @@
 class Comments::FavsCommentsController < ApplicationController
+  before_action :save_previous_url
+
+  def create
+    @fav_comment = FavsComment.new(comment_id: params[:comment_id], user_id: current_user.id)
+    @fav_comment.save
+    @back_url = session[:previous_url]
+
+    redirect_to @back_url
+  end
+
+  def destroy
+    @fav_comment = FavsComment.find_by(comment_id: params[:comment_id], user_id: current_user.id)
+    @fav_comment.destroy
+    @back_url = session[:previous_url]
+    redirect_to @back_url
+  end
+
+  def save_previous_url
+    session[:previous_url] = URI(request.referer || '').path
+  end
 end
