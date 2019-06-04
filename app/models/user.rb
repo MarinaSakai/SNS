c5ef1e0b19a4fee3f5dc1035860eccff56e13c00
Marina S <marina_s@fukazawa.info.waseda.ac.jp>
2019-06-04 23:45:25 +0900

    Show the list of follows and followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
+  validates :name, length: { maximum: 12 }
   validates :email, presence: true
 end
