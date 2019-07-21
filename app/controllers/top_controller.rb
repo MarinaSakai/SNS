4a2147e1ebf44cd3174ec9079df812c4b1cf897f
Marina S <marina_s@fukazawa.info.waseda.ac.jp>
2019-07-22 03:14:45 +0900

    changed the order to desc

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -4,6 +4,6 @@ class TopController < ApplicationController
     if current_user
       redirect_to posts_path
     end
-    @posts = Post.where(scope_of_disclosure: 'everyone')
+    @posts = Post.where(scope_of_disclosure: 'everyone').order(created_at: 'desc')
   end
 end
